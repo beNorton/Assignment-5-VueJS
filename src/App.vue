f963ac66a8bea85c45766cba941a95472d2b7496
Brice Hennelly <bricenorton@fas.harvard.edu>
2026-04-16 20:42:09 -0400

    removed some of the HelloWorld stuff. Added Bootstrap. Created new MealGallery component. Added a donut favicon.  Updated App.vue to use new component.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import MealGallery from './components/MealGallery.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <MealGallery />
 </template>
---
+++ src/components/MealGallery.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { meals } from '../services/MealData'
+</script>
+
+<template>
+  <div class="container">
+    <h1>Meal Gallery</h1>
+    <div class="row">
+      <div v-for="meal in meals" :key="meal._id" class="col-md-4">
+        {{ meal.mealname }}
+      </div>
+    </div>
+  </div>
+</template>
